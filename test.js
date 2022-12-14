0749ed18267d0aa6baa9f50e153d2fbf0e84b049
Kondratenko Konstia <evpatyikalovrat@gmail.com>
2022-12-13 22:52:49 -0500

    first commit 13/12/2022/

---
+++ test.js
@@ -0,0 +1,84 @@
+// date 13.12 2022
+//commet
+/* commet*/
+
+// let user = {
+//    name: 'vova',
+//    age: 43
+
+// };
+// // console.log(user.rest);
+// console.log(JSON.stringify(user));
+
+// var a;
+// a = 7;
+// var b;
+// b = a;
+
+// // Оголошення змінної
+// var studlyCapVar;
+// var properCamelCase;
+// var titleCaseOver;
+
+// // Призначення змінної
+//  studlyCapVar = 10;
+//  properCamelCase = "A String";
+//  titleCaseOver = 9000;
+
+
+
+// function func(int,int1) {
+//    return int + int1;
+   
+// }
+
+
+
+// console.log(func(1,1));
+
+// func = 'vova';
+
+// console.log(func);
+
+
+// gen = function() {
+//    var a = 123;
+//    return function() {
+//      return a + 1;
+//    }
+//   }
+
+//   console.log(gen());
+//   gen.a = 0;
+//   console.log(gen.a)
+
+//  let User = {
+//    age: 23,
+//    name: 'acab'
+//  }
+
+// console.log(typeof(User));
+
+// User = 0;
+// console.log(typeof(User));
+
+// function f(){
+//    return 1 + 5;
+// }
+// console.log(f());
+// f = 'Byba';
+// console.log(f);
+
+
+
+// const a = () => {
+//    return 5 + 5;
+// }
+
+// console.log(a());
+
+// a = 'byba';
+
+// console.log(a());
+
+
