fb9f1088d9489627cdfeed54d2b017a84445bd2a
Kondratenko Konstia <evpatyikalovrat@gmail.com>
2023-01-11 18:30:36 -0500

    11/01

--- test.js
+++ test.js
@@ -580,19 +580,25 @@
 
 
 
-Date.prototype.getDate = function getGrinchDate() {
-  let  arrDate=  this.toString().split(" ")
-console.log(arrDate)
-    if(arrDate[1] == "Dec" && arrDate[2] == '25'){
-      return 26
-    }else {
-      return Number(arrDate[2])+1
-    }      
+// Date.prototype.getDate = function getGrinchDate() {
+//   let  arrDate=  this.toString().split(" ")
+// console.log(arrDate)
+//     if(arrDate[1] == "Dec" && arrDate[2] == '25'){
+//       return 26
+//     }else {
+//       return Number(arrDate[2])+1
+//     }      
 
   
-}
+// }
 
 
-const d = new Date('1957-12-25');
-console.log(d.getDate())
+// const d = new Date('1957-12-25');
+// console.log(d.getDate())
 // console.log(d.getMonth())
+
+
+
+console.log(this)
+
+
